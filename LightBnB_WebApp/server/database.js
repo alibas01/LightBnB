05222a08b2efbfc90ea45b85fb800755d167a81f
Ali Bas <alibas01@gmail.com>
2021-02-04 18:10:31 -0500

    search page addition

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -134,11 +134,50 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-    SELECT *
+    // 1
+    const queryParams = [];
+    // 2
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
-    LIMIT $1;
-    `, [`${limit}`])
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+    // 3
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+    if (options.owner_id) {
+      queryParams.push(options.owner_id);
+      queryString += `AND owner_id LIKE $${queryParams.length} `;
+    }
+    if (options.minimum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night/100);
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
+    }
+    if (options.maximum_price_per_night) {
+      queryParams.push(options.maximum_price_per_night/100);
+      queryString += `AND cost_per_night <= $${queryParams.length} `;
+    }
+    if (options.minimum_rating) {
+      queryParams.push(Number(options.minimum_rating));
+      queryString += `AND rating >= $${queryParams.length} `;
+    }
+  
+    // 4
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+    // 5
+    console.log(queryString, queryParams);
+  
+    // 6
+    return pool.query(queryString, queryParams)
   .then(data => data.rows)
   .catch(err => console.error('query error', err.stack));
 }
